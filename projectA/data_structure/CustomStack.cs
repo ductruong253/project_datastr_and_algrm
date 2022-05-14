87b50ee32afb0873ead621cd050650149cb5d528
ductruong253 <ddtruong94@gmail.com>
2022-05-14 20:55:12 +0700

    First initial and add Node, CustomStack class

---
+++ projectA/data_structure/CustomStack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectA.data_structure
+{
+    internal class CustomStack<T>
+    {
+        private int count;
+        private Node<T> head;
+
+        public CustomStack() { }
+
+        public void Push(T value)
+        {
+            Node<T> node = new Node<T>(value, null);
+        }
+    }
+}
---
+++ projectA/data_structure/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectA.data_structure
+{
+    internal class Node<T>
+    {
+        private T data;
+        private Node<T> next;
+
+        public Node(){}
+        public Node(T data, Node<T> next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+
+        public T Data;
+        public Node<T> Next;
+
+        public Boolean hasNext()
+        {
+            return next != null;
+        }
+    }
+}
